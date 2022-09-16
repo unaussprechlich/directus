45187e697c16b01e4dae82eafff6026802c5c802
brainslug <tim@brainslug.nl>
2022-09-16 19:46:25 +0200

    fixed missed imports

--- api/src/services/websocket.ts
+++ api/src/services/websocket.ts
@@ -1,4 +1,4 @@
-import { getWebsocketController, WebsocketController } from '../websocket';
+import { getWebsocketController, WebsocketController } from '../websocket/controllers';
 import { WebsocketClient, WebsocketMessage } from '../websocket/types';
 import { stringify } from '../websocket/utils/message';
 import emitter from '../emitter';
@@ -19,7 +19,7 @@ export class WebsocketService {
 	}
 
 	broadcast(message: string | WebsocketMessage, filter?: { user?: string; role?: string }) {
-		this.controller.clients.forEach((client) => {
+		this.controller.clients.forEach((client: WebsocketClient) => {
 			if (filter && filter.user && filter.user !== client.accountability.user) return;
 			if (filter && filter.role && filter.role !== client.accountability.role) return;
 			client.send(typeof message === 'string' ? message : stringify(message));
