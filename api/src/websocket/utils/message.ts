1061a111cc8fefdd9ffcc82e3fd7c7e45de0862f
brainslug <tim@brainslug.nl>
2022-09-16 16:00:08 +0200

    rate limited send function

--- api/src/websocket/utils/message.ts
+++ api/src/websocket/utils/message.ts
@@ -1,3 +1,5 @@
+import { WebsocketClient } from '../types';
+
 /**
  * Message utils
  */
@@ -8,3 +10,17 @@ export const fmtMessage = (type: string, data: Record<string, any> = {}) => {
 	return JSON.stringify({ type, ...data });
 };
 export const errorMessage = (error: any) => JSON.stringify({ error });
+
+// we may need this later for slow connections
+export const safeSend = async (client: WebsocketClient, data: string, delay = 100) => {
+	if (client.readyState !== client.OPEN) return;
+	if (client.bufferedAmount > 0) {
+		// wait for the buffer to clear
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				safeSend(client, data, delay).finally(() => resolve(null));
+			}, delay);
+		});
+	}
+	client.send(data);
+};
